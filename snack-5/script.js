921a74e0cdf7265711e8bcb14cd9d6a0a0582c54
Nicolebalzano <nicole.balzano03@gmail.com>
2025-05-08 19:51:38 +0200

    feat: snack 5 completed

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,11 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let evenNum = [];
+const evenNumbers = nums.filter((curNum) => {
+    if(curNum % 2 === 0){
+evenNum.push(curNum);
+    }
+}) 
+console.log(evenNum);
